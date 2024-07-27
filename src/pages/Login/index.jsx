27417d6424ffdfedc65d36a87b8a84a0847c0d51
shooozz <vodolb@mail.ru>
2024-07-27 23:18:53 +0300

    fix login and register default value

--- src/pages/Login/index.jsx
+++ src/pages/Login/index.jsx
@@ -20,8 +20,8 @@ export const Login = () => {
         formState: { errors, isValid },
     } = useForm({
         defaultValues: {
-            email: "admin@gmail.com",
-            password: "admin",
+            email: "",
+            password: "",
         },
     });
 
