254d0069240a1e3ba46c53b5d0d76067ad1968e6
shooozz <vodolb@mail.ru>
2024-08-02 23:37:41 +0300

    fix baseURL

--- src/axios.ts
+++ src/axios.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-    baseURL: "http://localhost:4444/",
+    baseURL: "https://backend-blog-gules.vercel.app/",
 });
 
 instance.interceptors.request.use((config) => {
